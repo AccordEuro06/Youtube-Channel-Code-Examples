bc9ce2938d420e8866756b3fe0e7d021c402f2e4
thegrid22593 <garrett.sanderson@gmail.com>
2016-02-01 14:03:42 -0800

    to-do-list-start

--- to-do-list/js/app.js
+++ to-do-list/js/app.js
@@ -13,18 +13,31 @@
 
       cacheDom: function() {
         this.$button = $('#add-item-submit');
+        this.$list = $('.list');
+        this.$input = $('#add-item-input');
+        this.$completedList = $('.completed-list');
       },
 
       bindEvents: function() {
         console.log('bind');
-        this.$button.on('click', this.addNewToDo);
+        this.$button.on('click', this.addNewToDo.bind(this));
+        this.$list.on('change', 'input[type="checkbox"]', this.completeTask);
       },
 
       addNewToDo: function() {
-        var newToDo = $('#add-item-input').val();
+          var newToDo = this.$input.val();
+          this.$list.prepend('<li><input id="complete-check" type="checkbox">' + newToDo + '</li>');
+          this.$input.val('');
+      },
 
+      completeTask: function() {
+        $(this).parent().remove();
+        var completedValue = $(this).parent().text();
+        console.log(completedValue);
 
+        $('.completed-list').prepend('<li>'+ completedValue +'</li>');
       }
+
     };
 
     toDoList.init();
